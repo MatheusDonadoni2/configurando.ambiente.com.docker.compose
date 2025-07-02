9a0c6cb8d8de473dd8ad2988e1e3453cd49749f4
Matheus Donadoni <matheusgoncalvesdonadoni@gmail.com>
2025-07-02 13:58:35 -0300

    volumes | network | database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'myPOSTGRES_USER',
+      password: 'myPOSTGRES_PASSWORD',
+      database: 'myPOSTGRES_DB',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
